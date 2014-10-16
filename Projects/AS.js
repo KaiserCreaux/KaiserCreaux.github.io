12fa898f4b90aef6620456f6522a76577d062f89
KaiserCreaux <creaux@gmail.com>
2014-10-16 17:23:44 -0400

    Bug-swatting atempt

--- Projects/AS.js
+++ Projects/AS.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    $selectorList = $(".selectorList li");//this manages the three main pages of the site
+    $tabs = $(".tabs li");//this manages the three main pages of the site
     
-    $selectorList.click(function() {
+    $tabs.click(function() {
         if($(this)){
-            $selectorList.removeClass("selected");//makes sure nothing can retain 'selected' status after a new click input
+            $tabs.removeClass("selected");//makes sure nothing can retain 'selected' status after a new click input
         $(this).addClass("selected");}//re-establishes 'selected' on active li
         
         selectionId = $(this).attr('id');
         
-        $seg = $(".seg#"+selectionId);//creates object from whichever element of class seg was clicked by %selectorList
+        $seg = $(".seg#"+selectionId);//creates object from whichever element of class seg was clicked by %tabs
     $(".mContent").fadeOut("fast", function(){//removes active main content page from view, clearing the way for the called main content page
         $(".seg").css("display", "none");//gives all .seg class elements invisibility powers
         $(".seg#"+selectionId).css("display", "block");//brings the .seg class that is linked to selectionId back into view
